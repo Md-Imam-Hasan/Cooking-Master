e39ac04fc13020891a0cd7397ab93b1f90298df3
Hasan <imamhasan0007@gmail.com>
2021-02-07 07:39:42 +0600

    code organized

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 const showFoodIngredients = document.getElementById("show-food-ingredients");
@@ -9,52 +8,62 @@ const init = () => {
   showFood.className = '';
   showFood.innerHTML = '';
 }
-/// api URL ///
+// api URL 
 const apiURL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
-/// adding event listener in form
+// adding event listener in form
 form.addEventListener('submit', (e) => {
   const searchValue = search.value.trim();
   e.preventDefault();
   if (!searchValue) {
-    alert("There is nothing to search")
+    alert("There is nothing to search");
   }
   else {
-    searchFood(searchValue)
+    searchFood(searchValue);
   }
 });
 
+// Search Food
 const searchFood = async searchValue => {
   const searchResult = await fetch(`${apiURL}${searchValue}`);
   const data = await searchResult.json();
-  displayFoods(data.meals);
+  displayFoods(data);
 }
 
+// Displaying Food Items
 const displayFoods = foodItems => {
   init();
-  if (foodItems === null) {
-    const errorMessage = `
-    <div class="text-center">
-      <h1>This Food Recipe Is Not Available Now </h1>
-    </div>`;
-    showFood.innerHTML = errorMessage;
+  if (foodItems.meals === null) {
+    errorMessage();
   } else {
-    showFood.className = 'showing-food';
-    foodItems.forEach(foodItem => {
-      const foodItemDiv = document.createElement('div');
-      foodItemDiv.className = 'food-item';
-      const foodItemInfo = `
-        <a onclick="displayFoodIngredients('${foodItem.strMeal}')" class="food-ingredients-link" href="#show-food-ingredients">
-          <img src="${foodItem.strMealThumb}" class="food-image img-fluid" alt="food-image">
-          <h5 class="food-name my-3">${foodItem.strMeal}</h5>
-        </a>
-        `;
-      foodItemDiv.innerHTML = foodItemInfo;
-      showFood.appendChild(foodItemDiv);
-    });
+    renderFoodItems(foodItems.meals);
   }
 }
 
+const errorMessage = () => {
+  const message = `
+    <div class="text-center">
+      <h1>Sorry, This Food Recipe Is Not Available Now </h1>
+    </div>`;
+  showFood.innerHTML = message;
+}
+
+const renderFoodItems = foodItemMeals => {
+  showFood.className = 'showing-food';
+  foodItemMeals.forEach(foodItem => {
+    const foodItemDiv = document.createElement('div');
+    foodItemDiv.className = 'food-item';
+    const foodItemInfo = `
+      <a onclick="displayFoodIngredients('${foodItem.strMeal}')" href="#show-food-ingredients">
+        <img src="${foodItem.strMealThumb}" class="food-image img-fluid" alt="food-image">
+        <h5 class="food-name my-3">${foodItem.strMeal}</h5>
+      </a>`;
+    foodItemDiv.innerHTML = foodItemInfo;
+    showFood.appendChild(foodItemDiv);
+  });
+}
+
+// Displaying Food Ingredients
 const displayFoodIngredients = async foodName => {
   const foodItem = await fetch(`${apiURL}${foodName}`);
   const data = await foodItem.json();
@@ -64,21 +73,24 @@ const displayFoodIngredients = async foodName => {
 const renderFoodIngredients = foodItem => {
   showFoodIngredients.style.display = 'block';
   showFoodIngredients.innerHTML = `
-    <img src="${foodItem.strMealThumb}" class="food-image img-fluid mb-4" alt="">
+    <img src="${foodItem.strMealThumb}" class="food-image img-fluid mb-4" alt="food-image">
     <div class="ms-4">
       <h2 class="food-name mb-3">${foodItem.strMeal}</h2>
       <h5 >Ingredients</h5>
-    </div>
-    `;
-  for (let i = 1; i <= 20; i++) {
+    </div>`;
+    addIngredientList(foodItem);
+}
+
+const addIngredientList = foodItem =>{
+  for (let i = 1; ; i++) {
     const ingredient = 'strIngredient' + i;
     const measure = 'strMeasure' + i;
     if (!foodItem[ingredient]) break;
     else {
       const ingredients = document.createElement("div");
-      ingredients.className = 'ms-4'
+      ingredients.className = 'ms-4';
       const ingredientInfo = `
-      <span><i class="fas fa-check-square icon"> </i></span> ${foodItem[ingredient]} ${foodItem[measure]}<span> </span>`;
+      <span><i class="fas fa-check-square icon"></i></span>&nbsp&nbsp ${foodItem[ingredient]} ${foodItem[measure]}<span> </span>`;
       ingredients.innerHTML = ingredientInfo;
       showFoodIngredients.appendChild(ingredients);
     }
